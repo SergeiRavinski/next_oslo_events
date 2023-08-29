0efb1eb0c33df9b692330db983c4af2cf8252eb4
sergei <ravinski.sergei@gmail.com>
2023-08-29 15:35:04 +0200

    Created tsx structure

--- components/events/event_list.js
+++ components/events/event_list.js
@@ -2,3 +2,17 @@ import EventItem from './event_item';
 
 import classes from './event_list.module.css';
 
+	return (
+		<ul className={classes.list}>
+			{items.map((event) => (
+				<EventItem
+					key={event.id}
+					id={event.id}
+					title={event.titile}
+					location={event.location}
+					date={event.date}
+					image={event.image}
+				/>
+			))}
+		</ul>
+	);
