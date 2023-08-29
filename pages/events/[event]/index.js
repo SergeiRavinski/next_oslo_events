8874f7b6dcf98ca4291ffe20740f9f4afc38dfa8
sergei <ravinski.sergei@gmail.com>
2023-08-29 12:48:40 +0200

    Created pages

---
+++ pages/events/[event]/index.js
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router';
+
+function EventDetailPage() {
+	const router = useRouter();
+
+	return (
+		<div>
+			<h1>The Event Detail Page</h1>
+		</div>
+	);
+}
+
+export default EventDetailPage;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function StartingPage() {
+	return (
+		<div>
+			<h1>The Starting Page</h1>
+		</div>
+	);
+}
+
+export default StartingPage;
