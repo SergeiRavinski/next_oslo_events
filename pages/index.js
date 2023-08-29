aaa8618cb348d91217aa5097d69f9202321a3fe9
sergei <ravinski.sergei@gmail.com>
2023-08-29 15:38:04 +0200

    Imported and added the EventList component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import { getFeaturedEvents } from '../dummy_data';
+import EventList from '../components/events/event_list';
+
 function HomePage() {
 	return (
 		<div>
-			<h1>The Home Page</h1>
+			<EventList items={featuredEvents} />
 		</div>
 	);
 }
