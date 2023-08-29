d8c1e0045d74ac1425c72a0d263f7862562b10bb
sergei <ravinski.sergei@gmail.com>
2023-08-29 15:24:46 +0200

    Created jsx structure

--- components/events/event_item.js
+++ components/events/event_item.js
@@ -1,3 +1,26 @@
 import Link from 'next/link';
 
 import classes from './event_item.module.css';
+
+	return (
+		<li className={classes.item}>
+			<img src={'/' + image} alt={id} />
+			<div className={classes.content}>
+				<div className={classes.summary}>
+					<h2>{title}</h2>
+					<div className={classes.date}>
+						<time>{readeableDate}</time>
+					</div>
+					<div className={classes.address}>
+						<address>{formattedAddress}</address>
+					</div>
+				</div>
+				<div className={classes.actions}>
+					<Link href={exploreLink}>Explore Event</Link>
+				</div>
+			</div>
+		</li>
+	);
+}
+
+export default EventItem;
