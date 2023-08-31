3671637767c26a7be57ad26d7bc85a4b459c90d9
sergei <ravinski.sergei@gmail.com>
2023-08-31 11:07:39 +0200

    Imported the LogisticsItem component

--- components/event_detail/event_logistics.js
+++ components/event_detail/event_logistics.js
@@ -1,3 +1,4 @@
 import AddressIcon from '../icons/address_icon';
 import DateIcon from '../icons/date_icon';
+import LogisticsItem from './logistics_item';
 import classes from './event_logistics.module.css';
