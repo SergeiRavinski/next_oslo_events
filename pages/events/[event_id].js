d760eeac86977222fff1e82218444c8129ae81a6
sergei <ravinski.sergei@gmail.com>
2023-08-31 11:02:37 +0200

    Imported the EventLogistics component

--- pages/events/[event_id].js
+++ pages/events/[event_id].js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 
 import { getEventById } from '../../dummy_data';
 import EventSummary from '../../components/event_detail/event_summary';
+import EventLogistics from '../../components/event_detail/event_logistics';
 function EventDetailPage() {
 	const router = useRouter();
 	if (!event) {
