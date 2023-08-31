3f54dcd5feb8b23dbde5307afd49259536bffac5
sergei <ravinski.sergei@gmail.com>
2023-08-31 14:52:42 +0200

    Imported and added the useRouter hook

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,12 @@
 import { Fragment } from 'react';
+import { useRouter } from 'next/router';
 
 import { getAllEvents } from '../../dummy_data';
 import EventList from '../../components/events/event_list';
 import EventsSearch from '../../components/events/event_search';
 
 function AllEventsPage() {
+	const router = useRouter();
 	const events = getAllEvents();
 
 	return (
