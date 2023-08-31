facd9878c3a7e97a98d02218b2b4b9ef7f90814b
sergei <ravinski.sergei@gmail.com>
2023-08-31 11:43:35 +0200

    Created the MainHeader component

--- components/layout/main_header.js
+++ components/layout/main_header.js
@@ -1,3 +1,22 @@
 import Link from 'next/link';
 
 import classes from './main_header.module.css';
+
+function MainHeader() {
+	return (
+		<header className={classes.header}>
+			<div className={classes.logo}>
+				<Link href='/'>Oslo Events</Link>
+			</div>
+			<nav className={classes.navigation}>
+				<ul>
+					<li>
+						<Link href='/events'>All Events</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+}
+
+export default MainHeader;
