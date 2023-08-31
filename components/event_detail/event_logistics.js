528443719aa16eae6f40f42ab9a0dba60d7e3133
sergei <ravinski.sergei@gmail.com>
2023-08-31 11:09:23 +0200

    Created function to format address and date

--- components/event_detail/event_logistics.js
+++ components/event_detail/event_logistics.js
@@ -2,3 +2,10 @@ import AddressIcon from '../icons/address_icon';
 import DateIcon from '../icons/date_icon';
 import LogisticsItem from './logistics_item';
 import classes from './event_logistics.module.css';
+
+	const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric',
+	});
+	const addressText = address.replace(', ', '\n');
