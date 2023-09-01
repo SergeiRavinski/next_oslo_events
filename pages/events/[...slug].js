1088e16133c29c2edccd178d9a9da35dab564f4e
sergei <ravinski.sergei@gmail.com>
2023-09-01 14:58:24 +0200

    Imported hooks and components

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,3 +1,12 @@
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+
+import { getFilteredEvents } from '../../dummy_data';
+import EventList from '../../components/events/event_list';
+import ResultsTitle from '../../components/events/results_title';
+import Button from '../../components/ui/button';
+import ErrorAlert from '../../components/ui/error_alert';
+
 function FilteredEventsPage() {
 	return (
 		<div>
