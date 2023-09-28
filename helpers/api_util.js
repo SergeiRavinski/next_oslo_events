294b5f7bb5e49bf73092d62563321e19da11d345
sergei <ravinski.sergei@gmail.com>
2023-09-28 14:35:22 +0200

    Created the getAllEvents function

---
+++ helpers/api_util.js
@@ -0,0 +1,16 @@
+export async function getAllEvents() {
+	const response = await fetch(
+		'https://nextjs-course-af7da-default-rtdb.firebaseio.com/events.json'
+	);
+	const data = await response.json();
+	const events = [];
+
+	for (const key in data) {
+		events.push({
+			id: key,
+			...data[key],
+		});
+	}
+
+	return events;
+}
